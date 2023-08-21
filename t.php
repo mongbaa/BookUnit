863cf9a1202448003158634e64e70efeba53fdce
มงคล ทองเพชรคง <mongkol.th@psu.ac.th>
2023-08-21 11:00:29 +0700

    index

--- t.php
+++ t.php
@@ -1,7 +1,15 @@
-git commit -m "first commit"
+git status .
+git pull
+git add .
+git commit -m 'index'
+git push
 
-
-git add . 
+echo "# BookUnit" >> README.md
+git init
+git add README.md
 git commit -m "first commit"
+git branch -M main
 git remote add origin https://github.com/mongbaa/BookUnit.git
 git push -u origin main
+
+
